edfd08e0733216ba0012391bf71e9358fc5df06a
Aya Heshmat <ayaheshmatpmd@gmail.com>
2019-06-29 13:34:42 -0400

    Loading HW3

--- Python Hw 3/PyBank/main.py
+++ Python Hw 3/PyBank/main.py
@@ -6,3 +6,45 @@ Created on Mon Jun 24 18:19:14 2019
 @author: Aya
 """
 
+import os 
+import csv
+
+pybank_csv = os.path.join('budget_data.csv')
+
+numMonths = []
+netTotal = []
+monthlyChange = []
+
+with open(pybank_csv, "r", newline="") as budget:
+   csvreader = csv.reader(budget,delimiter=",")
+   header = next(csvreader)
+   
+   for row in csvreader:
+       numMonths.append(row[0])
+       netTotal.append(int(row[1]))
+    
+   for i in range(len(netTotal)-1):
+       monthlyChange.append(netTotal[i+1]-netTotal[i])
+
+maxIncrease = max(monthlyChange)
+maxDecrease = min(monthlyChange)
+
+maxIncreaseMonth = monthlyChange.index(max(monthlyChange))+1
+maxDecreaseMonth = monthlyChange.index(min(monthlyChange))+1
+
+analysis = f'''
+Financial Summary Analysis
+--------------------------
+Total Months: {len(numMonths)}
+Total: ${sum((netTotal))}
+Average Change: {round(sum(monthlyChange)/len(monthlyChange),2)}
+Greatest Increase in Profits: {numMonths[maxIncreaseMonth]}(${(str(maxIncrease))})
+Greatest Decrease in Profits: {numMonths[maxDecreaseMonth]}(${(str(maxDecrease))})
+
+'''
+
+output_file = os.path.join("analysis.txt")
+
+with open(output_file, "w") as file:
+    file.write(analysis)
+
--- Python Hw 3/PyPoll/main.py
+++ Python Hw 3/PyPoll/main.py
@@ -6,3 +6,68 @@ Created on Mon Jun 24 18:19:14 2019
 @author: Aya
 """
 
+import os
+import csv
+
+election_csv = os.path.join("election_data.csv")
+
+totalVotes = 0
+khanVotes = 0
+correyVotes = 0
+liVotes = 0
+otooleyVotes = 0
+
+with open(election_csv, "r", newline="") as elections:
+    csvreader = csv.reader(elections, delimiter=",")
+    header = next(csvreader)
+    
+    for row in csvreader:
+        totalVotes +=1
+        
+        if row[2] == "Khan":
+            khanVotes +=1
+            
+        elif row[2] == "Correy":
+            correyVotes +=1
+        
+        elif row[2] == "Li":
+            liVotes +=1
+        
+        elif row[2] == "O'Tooley":
+            otooleyVotes +=1
+
+candidates = ["Khan","Correy","Li","O'Tooley"]
+votes = [khanVotes, correyVotes, liVotes, otooleyVotes]
+
+candNvotes = dict(zip(candidates,votes))
+
+key = max(candNvotes, key=candNvotes.get)
+
+#summary of analysis
+khanPercent = (khanVotes/totalVotes)*100
+correyPercent = (correyVotes/totalVotes)*100
+liPercent = (liVotes/totalVotes)*100
+otooleyPercent = (otooleyVotes/totalVotes)*100
+
+#SUMMARY TABLE
+analysis = f'''
+Election Results
+----------------
+Total Votes: {totalVotes}
+----------------
+Khan: {khanPercent:.3f}% ({khanVotes})
+Correy: {correyPercent:.3f}% ({correyVotes})
+Li: {liPercent:.3f}% ({liVotes})
+O'Tooley: {otooleyPercent:.3f}% ({otooleyVotes})
+----------------
+Winner: {key}
+----------------
+'''
+print(analysis)
+
+#OUTPUT FILES
+outputFile = os.path.join("election_results.csv")
+
+with open(outputFile, "w") as file:
+    file.write(analysis)
+
